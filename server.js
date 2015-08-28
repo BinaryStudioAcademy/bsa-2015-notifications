87abf8e05797484690490e71e3718d2b876599b3
Mykyta Semenistyi <nikeiwe@gmail.com>
2015-08-28 19:43:48 +0300

    add CORS headers

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use('/', express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
+app.use(function(req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "DELETE,GET,POST,PUT");
+	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	return next();
+});
+
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
