407519004b83dc7049cdbc2e13a621c421c74220
YuriyPotanin <yurick85@gmail.com>
2015-08-31 15:36:36 +0300

    Fix main header

--- frontend/javascripts/header/header.js
+++ frontend/javascripts/header/header.js
@@ -1,4 +1,4 @@
-function headerFubction(){
+function headerFunction() {
     document.getElementById('appsBtn').addEventListener('click', function() {
         document.getElementById("notificationBlock").classList.add('invisible');
         var block = document.getElementById("appsBlock");
@@ -11,12 +11,49 @@ function headerFubction(){
         block.classList.toggle('invisible');
     });
 
+    var userName = document.getElementById("userName");
+    var avatar = document.getElementById('avatar');
+    var userLink = document.getElementById('userLink');
+
+    function getCookie(name) {
+        var matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+    var serverUID = getCookie('serverUID');
+    var userObject = {};
+
+    var getUser = function() {
+        var request = new XMLHttpRequest();
+        request.open('GET', 'http:team.binary-studio.com/profile/api/users/?serverUserId=' + serverUID, true);
+        request.withCredentials = true;
+        request.send();
+        request.onreadystatechange = function() {
+            if (request.readyState != 4) return;
+            if (request.status != 200) {
+                alert(request.status + ': ' + request.statusText);
+            } else {
+                userObject = JSON.parse(request.responseText)[0];
+                avatar.setAttribute("src", 'http:team.binary-studio.com/profile' + userObject.avatar.urlAva);
+                userName.innerHTML = userObject.name + ' ' + userObject.surname;
+                userLink.setAttribute('href', 'http:team.binary-studio.com/profile');
+            }
+        };
+    };
+
+    getUser();
+
+    //=======================================
+
+
     var notificationList = document.getElementById("notificationList");
     var respArray = [];
 
     var getNotification = function() {
         var request = new XMLHttpRequest();
-        request.open('GET', 'http://team.binary-studio.com/app/api/notification', true);
+        request.open('GET', 'http://team.binary-studio.com/app/api/usernotification/' + userObject.id, true);
         request.send();
         request.onreadystatechange = function() {
             if (request.readyState != 4) return;
@@ -24,6 +61,7 @@ function headerFubction(){
                 alert(request.status + ': ' + request.statusText);
             } else {
                 respArray = JSON.parse(request.responseText);
+                console.log(respArray);
                 addNotification(respArray);
             }
         };
@@ -57,8 +95,9 @@ function headerFubction(){
             renderNotification(notiObj);
         });
     };
-    var servicesList = document.getElementById("appsBlock");
-    var respArrayServise = [];
+
+    var servicesList = document.getElementById("appsList");
+    appsArray = [];
 
     var getService = function() {
         var request = new XMLHttpRequest();
@@ -69,8 +108,8 @@ function headerFubction(){
             if (request.status != 200) {
                 alert(request.status + ': ' + request.statusText);
             } else {
-                respArrayServise = JSON.parse(request.responseText);
-                addService(respArrayServise);
+                appsArray = JSON.parse(request.responseText);
+                addService(appsArray);
             }
         };
     };
@@ -89,4 +128,79 @@ function headerFubction(){
             renderServices(notiObj);
         });
     };
+    var inputField = document.getElementById('searchBar');
+    var searchList = document.getElementById('search');
+    var inputValue = "";
+    var usersArray = [];
+    var searchFiltrRsult = [];
+
+    inputField.addEventListener('focus', function() {
+        {
+            getAllUsers();
+
+            console.log(getAllUsers);
+        }
+    });
+    inputField.addEventListener('keyup', function(e) {
+
+        getInputValue();
+        filtrByInput(usersArray, inputValue);
+        renderSearchResult(searchFiltrRsult);
+        if (inputValue === '') {
+            searchFiltrRsult = [];
+            searchList.innerHTML = '';
+        }
+
+    });
+
+    var getAllUsers = function() {
+        var request = new XMLHttpRequest();
+        request.open('GET', 'http://team.binary-studio.com/profile/api/users', true);
+        request.withCredentials = true;
+        request.send();
+        request.onreadystatechange = function() {
+            if (request.readyState != 4) return;
+            if (request.status != 200) {
+                alert(request.status + ': ' + request.statusText);
+            } else {
+                usersArray = JSON.parse(request.responseText);
+                console.log(usersArray);
+
+            }
+        };
+    };
+
+    var filtrByInput = function(array, inputValue) {
+        searchFiltrRsult = [];
+        var reg = new RegExp(inputValue, 'i');
+        array.forEach(function(user) {
+            var tmpNS = user.name + ' ' + user.surname;
+
+            var tmpSN = user.surname + ' ' + user.name;
+
+            if (user.name.search(reg) !== -1 || user.surname.search(reg) !== -1 || tmpSN.search(reg) !== -1 || tmpNS.search(reg) !== -1) {
+                searchFiltrRsult.push(user);
+            }
+        });
+
+    };
+    var renderSearchResult = function(resultArrey) {
+        searchList.innerHTML = '';
+        resultArrey.forEach(function(obj) {
+            renderItem(obj.name, obj.surname, obj.id);
+        });
+    };
+    var renderItem = function(name, surname, id) {
+        var searchItem = document.createElement('a');
+        searchItem.className = 'noTextDecoration';
+        var path = 'http://http://team.binary-studio.com/profile/api/users/' + id;
+        searchItem.setAttribute("href", path);
+        searchItem.innerHTML = '<div class = "searchItem"><span>' + name + ' ' + surname + '</span></div>';
+        searchList.appendChild(searchItem);
+    };
+    var getInputValue = function() {
+        inputValue = inputField.value;
+    };
+
+
 }
