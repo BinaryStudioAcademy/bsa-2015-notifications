f385e39fce4e0afeb861c7a55f738b04d07c34f5
Mykyta Semenistyi <nikeiwe@gmail.com>
2015-09-03 14:52:58 +0300

    add hosts to header

--- routes/view/common.js
+++ routes/view/common.js
@@ -2,13 +2,20 @@ var config = require('../../config/');
 module.exports = function (app) {
 
 	app.get('/', function(req, res){
-		res.render('index', {loginserver:config.loginserver.host,
-		notificationserver:config.notificationserver.host,
-		userprofileserver:config.userprofileserver.host});
+		res.render('index', {
+			loginserver:config.loginserver.host,
+			notificationserver:config.notificationserver.host,
+			userprofileserver:config.userprofileserver.host
+		});
 	});
-	app.get('/header', function(req, res){
-		res.header('Access-Control-Allow-Origin', '*');
-		res.render('header');
+	app.get('/header', {
+			loginserver:config.loginserver.host,
+			notificationserver:config.notificationserver.host,
+			userprofileserver:config.userprofileserver.host
+		}, 
+		function(req, res){
+			res.header('Access-Control-Allow-Origin', '*');
+			res.render('header');
 	});
 
 };
